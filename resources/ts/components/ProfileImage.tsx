e1ca0738ffa295d9d8bb868eaaecd964450ecd94
ahitotsui <atsushiabc1204@gmail.com>
2024-05-26 16:29:24 +0900

    fix:　ヘッダーの画像ファイルパス修正

--- resources/ts/components/ProfileImage.tsx
+++ resources/ts/components/ProfileImage.tsx
@@ -5,7 +5,6 @@ import { useState, useEffect, useContext } from "react";
 
 export const ProfileImage:FC<{width:number,height:number,}> = ({width,height}) =>{
 
-
     const [my_image,setMyImage] = useState<any>("");
     useEffect(()=>{
         // トークンでアクセスしてユーザー名を取得
@@ -22,7 +21,7 @@ export const ProfileImage:FC<{width:number,height:number,}> = ({width,height}) =
     return(
         <>
             {my_image != "" ?
-                <img src={ '/storage/images/profile/' + my_image} width={width} height={height} alt={my_image} className="block rounded-full border border-gray-400" />
+                <img src={'http://' +window.location.host+'/storage/images/profile/' + my_image} width={width} height={height} alt={my_image} className="block rounded-full border border-gray-400" />
                 :
                 <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" width={width} height={height} className="block rounded-full" />
             }
