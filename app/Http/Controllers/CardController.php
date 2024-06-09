9eda0f8a4491cccf788b672521b17b8ad6972b11
ahitotsui <atsushiabc1204@gmail.com>
2024-06-09 21:47:31 +0900

    fix 画像権限

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -72,8 +72,8 @@ class CardController extends Controller
             $img_path = basename($path);
 
             //reactからアクセスできるように権限付与
-            $file = storage_path();//storageのフルパスを取得
-            chmod($file, 0755);
+            $fullPath = Storage::path($path);//storageのフルパスを取得
+            chmod($fullPath, 0755);
         }elseif($image == null){
             //画像ファイルが無ければ、DBにファイル名をnullで保存
             $img_path = null;
